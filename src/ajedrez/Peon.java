82bc96f3e557df297fea2693030fa0add2e4687d
Santi <santywindows8@hotmail.com>
2018-02-28 13:37:56 +0100

    Inicio P

--- src/ajedrez/Peon.java
+++ src/ajedrez/Peon.java
@@ -33,6 +33,7 @@ public class Peon extends Pieza {
     @Override
     public boolean puedeMover(Movimiento mov, Tablero tablero) {
         boolean puedeMover = false;
+        
         if (color == 'B' && mov.movVertical()) {
             if (mov.saltoVertical() == -1) {
                 puedeMover = true;
@@ -40,15 +41,17 @@ public class Peon extends Pieza {
                 puedeMover = true;
             }
         }
+        
+        if(color == 'B' && mov.saltoVertical() == -1 && Math.abs(mov.saltoHorizontal()) == 1){
+            if(tablero.buscarPieza(mov.getPosFinal().getFila(), mov.getPosFinal().getColumna()) != null){
+                puedeMover = true;
+            }
+        }
+        
         if (color == 'N' && mov.movVertical()) {
             if (mov.saltoVertical() == 1) {
                 puedeMover = true;
             } else if (mov.saltoVertical() <= 2 && mov.saltoVertical() > 0 && mov.getPosInicial().getFila() == 1) {
-                puedeMover = true;
-            }
-        }
-        if(color == 'B' && mov.saltoVertical() == -1 && Math.abs(mov.saltoHorizontal()) == 1){
-            if(tablero.buscarPieza(mov.getPosFinal().getFila(), mov.getPosFinal().getColumna()) != null){
                 puedeMover = true;
             }
         }
